11cded6a427cb09bc13d2627ea355041e2a2abaa
lethanhdat_f1_hcm <dat.lt1@citigo.com.vn>
2021-12-14 18:04:28 +0700

    no message

--- HubService/HubService/Hubs/ChatHub.cs
+++ HubService/HubService/Hubs/ChatHub.cs
@@ -8,7 +8,7 @@ namespace HubService.Hubs
 {
     public class ChatHub : Hub
     {
-        public async Task SendMessage(string user, string message)
+        public async Task SendAllMessage(string user, string message)
         {
             await Clients.All.SendAsync("ReceiveMessage", user, message);
         }
